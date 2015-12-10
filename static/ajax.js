bcc1b5dbe79f2c94b85215132121061f9a488239
Sandy Fang <sfang2016@gmail.com>
2015-12-10 18:56:13 -0500

    FLICKR THINGSS

--- static/ajax.js
+++ static/ajax.js
@@ -9,6 +9,24 @@ var getData = function(){
 	});
 }
 
+// api key = 2d0f6fb08bbdc443a85afa2bb39823fa
+var searchurl =
+"https://api.flickr.com/services/rest/?method=flickr.photos.search& \
+api_key=fcf1732b2d4886c106cdbb3e13904ec1&tags=cats&format=json&nojsoncallback=1 \
+&per_page=1&safe_search=1";
+var contexturl =
+"https://api.flickr.com/services/rest/?method=flickr.photos.getInfo& \
+api_key=fcf1732b2d4886c106cdbb3e13904ec1&photo_id=23030495873&format=json&nojsoncallback=1";
+
+console.log("Attempting to access flickr");
+$.getJSON(searchurl,function(data){
+	$.getJSON(contexturl,function(data){
+		var flickrurl = data['photo']['urls']['url'][0]['_content'];
+		console.log(flickrurl);
+		$("#flickrhere").attr("src",flickrurl);
+	});
+});
+
 var searchCat = function searchCat(s){
 //    $.getJSON();
 };
@@ -25,3 +43,5 @@ $("#find").click(function(){
     console.log(data);
     searchCat(data);
 });
+
+
