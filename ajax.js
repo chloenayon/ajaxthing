7b6729031a07b978bc6cdf358b1035d306760164
Sandy Fang <sfang2016@gmail.com>
2015-12-09 23:38:06 -0500

    haha oops...

--- ajax.js
+++ ajax.js
@@ -19,36 +19,3 @@ var interval = setInterval(getData,5000);
 $("#stop").click(function(){
 	clearInterval(interval);
 });
-
-var stuffdemo = function(){
-
-		console.log("Calling getstuff");
-
-		$.get("/getstuff",function(d){
-				console.log("getstuff returned: "+d);
-		});
-
-
-		
-		console.log("back from getstuff");
-
-};
-
-var paramtest = function paramtest(){
-		$.getJSON("/upcase",{data:'hello'},function(d){
-				console.log(d);
-				console.log(d.result);
-		});
-
-};
-
-
-$("#b").click(function(){
-		var input = $("#data");
-		var d = input.val();
-		input.val("");
-		$.getJSON("/upcase",{data:d},function(d){
-				$("#result").text(d.result);
-				$("#thelist").append($("<li>"+d.result+"</li>"));
-		});
-});
--- app.py
+++ app.py
@@ -7,14 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/upcase")
-def upcase():
-    data = request.args.get("data")
-    print data
-    result = {'original' : data,
-            'result':data.upper()}
-    return json.dumps(result)
     
 if __name__ == "__main__":
    app.debug = True
