39b8f7d8918b6b03e8fd6f5ef81818b497d4ce5f
Sandy Fang <sfang2016@gmail.com>
2015-12-10 19:46:21 -0500

    jksdhfkds

--- static/ajax.js
+++ static/ajax.js
@@ -13,17 +13,18 @@ var getData = function(){
 var searchurl =
 "https://api.flickr.com/services/rest/?method=flickr.photos.search& \
 api_key=fcf1732b2d4886c106cdbb3e13904ec1&tags=cats&format=json&nojsoncallback=1 \
-&per_page=1&safe_search=1";
+&per_page=1&safe_search=1&extras=url_m";
 var contexturl =
 "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo& \
 api_key=fcf1732b2d4886c106cdbb3e13904ec1&photo_id=23030495873&format=json&nojsoncallback=1";
 
 console.log("Attempting to access flickr");
 $.getJSON(searchurl,function(data){
+	console.log(data);
+	$("#flickrhere").attr("src",data['photos']['photo'][0]['url_m']);
 	$.getJSON(contexturl,function(data){
 		var flickrurl = data['photo']['urls']['url'][0]['_content'];
-		console.log(flickrurl);
-		$("#flickrhere").attr("src",flickrurl);
+		console.log(data);
 	});
 });
 
@@ -35,6 +36,7 @@ console.log("Updating on Interval");
 var interval = setInterval(getData,5000);
 
 $("#stop").click(function(){
+	console.log("Interval Cleared");
 	clearInterval(interval);
 });
 
