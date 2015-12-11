4e77602c9f6d3a17a1fdf2cb14d7fa1d16f6beb4
Chloe Kaubisch <nemononiam@gmail.com>
2015-12-10 23:08:08 -0500

    it works

--- static/ajax.js
+++ static/ajax.js
@@ -10,26 +10,27 @@ var getData = function(){
 }
 
 // api key = 2d0f6fb08bbdc443a85afa2bb39823fa
-var searchurl =
+var searchurls =
 "https://api.flickr.com/services/rest/?method=flickr.photos.search& \
-api_key=fcf1732b2d4886c106cdbb3e13904ec1&tags=cats&format=json&nojsoncallback=1 \
+api_key=fcf1732b2d4886c106cdbb3e13904ec1&tags=cats,%s%&format=json&nojsoncallback=1 \
 &per_page=1&safe_search=1&extras=url_m";
 var contexturl =
 "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo& \
 api_key=fcf1732b2d4886c106cdbb3e13904ec1&photo_id=23030495873&format=json&nojsoncallback=1";
 
-console.log("Attempting to access flickr");
-$.getJSON(searchurl,function(data){
-	console.log(data);
-	$("#flickrhere").attr("src",data['photos']['photo'][0]['url_m']);
-	$.getJSON(contexturl,function(data){
-		var flickrurl = data['photo']['urls']['url'][0]['_content'];
-		console.log(data);
-	});
-});
-
 var searchCat = function searchCat(s){
-//    $.getJSON();
+    var params = s.replace(/ /g, ",");
+    var searchurl = searchurls.replace("%s%", params);
+    console.log(searchurl);
+    console.log("Attempting to access flickr");
+    $.getJSON(searchurl,function(data){
+	    console.log(data);
+	    $("#flickrhere").attr("src",data['photos']['photo'][0]['url_m']);
+	    $.getJSON(contexturl,function(data){
+		    var flickrurl = data['photo']['urls']['url'][0]['_content'];
+		    console.log(flickrurl);
+		});
+	});
 };
 
 console.log("Updating on Interval");
@@ -42,7 +43,6 @@ $("#stop").click(function(){
 
 $("#find").click(function(){
     var data = document.getElementById("params").value;
-    console.log(data);
     searchCat(data);
 });
 
